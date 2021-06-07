3c5ca3ce0829b270a6a34b705b3fc1196b8eeda4
terryproctor <terryproctor2015@outlook.com>
2021-06-07 21:56:23 +0100

    submit creating todos

--- src/index.js
+++ src/index.js
@@ -31,6 +31,8 @@ class List{
     addItem = (itemName) => this.items.push(itemName);
 }; 
 
+const createTodo = (title, description, dueDate, priority) => new Todo(title, description, dueDate, priority);
+
 // test
 // let bigProject = new Todo('birthday party', 'Georges birthday party', '1804');
 // bigProject.createList('go_shopping');
@@ -45,11 +47,40 @@ const domStuff = (() => {
     const mainContent = document.getElementById('content');
     const addTodo = document.getElementById('addTodo');
     const todoScreen = document.getElementById('todoScreen');
-    //plus sign toggle main screen display and form popping up
-    addTodo.addEventListener('click', () => {
+    const form = document.getElementById('todoForm');
+    const submit = document.getElementById('submit');
+
+    const getFormStuff = () => {
+        const newName = document.getElementById('todoName').value;
+            const newDate = document.getElementById('todoDueDate').value;
+            const newDescription = document.getElementById('description').value;
+            const priorityRadio = form.querySelectorAll('input[name=priority]');
+            let newPriority;
+            for (rBtn of priorityRadio) {
+                if (rBtn.checked) {newPriority = rBtn.value;}
+            }
+        return {newName, newDate, newDescription, newPriority}
+    }
+
+    const showForm = () => {
         mainContent.style.display="none";
         todoScreen.style.display='block';
-    });
-}
-)();
+    }
 
+    //plus sign toggle main screen display and form popping up
+    addTodo.addEventListener('click', (e) => {
+        e.preventDefault();
+        showForm();
+        
+        submit.addEventListener('click', (e)=> {
+            e.preventDefault();
+            //create object from form
+            const newForm = getFormStuff();
+            //use form to create Todo
+            const newTodo = createTodo(newForm.newName, newForm.newDescription, newForm.newDate, newForm.newPriority);
+            console.log(newTodo)
+        })
+    });
+
+    
+})();
